ec235761f956c7904e5e1e9bd7ffc2769f686c10
Ramya-talatam <108510824+Ramya-talatam@users.noreply.github.com>
2024-03-18 16:24:28 +0530

    Added detailed notes

--- Abstraction.java
+++ Abstraction.java
@@ -1,4 +1,17 @@
+/*
+Abstraction in Java is an oop concept that allows to represent the essential features of an object while hiding the implementation details.
+It focuses on what an object does rather than how it achieves its behavior.
 
+There are two main ways to implement abstraction in Java:
+Abstract Classes:
+An abstract class is a class that cannot be instantiated on its own and may contain abstract methods, which are methods without a body. 
+
+Interfaces: 
+An interface is a reference type in Java that contains only abstract methods, default methods, static methods, and constant declarations.
+Interfaces cannot be instantiated directly, but classes can implement interfaces, thereby agreeing to provide concrete implementations for all the methods declared in the interface.
+
+(Interfaces are more abstract than abstract classes as it does not allow to implement non abstract methods which means 100% abstraction)
+*/
 abstract class DataValley { 
 	private String name; 
 
